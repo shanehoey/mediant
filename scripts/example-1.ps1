24cd5d0625a95151aafdd60eda0b6cf403941e3b
shanehoey <shanehoey@users.noreply.github.com>
2018-01-08 23:20:31 +1000

    Added Examples

---
+++ scripts/example-1.ps1
@@ -0,0 +1,11 @@
+﻿#requires -version 5.0
+#requires -module Mediant
+
+import-module mediant 
+
+$mediantcredential = Get-Credential 
+
+$mediantdevice = Get-MediantDevice -Mediant 172.30.69.146 -Credential $mediantcredential -Http http
+
+Get-MediantDeviceFileIni -MediantDevice $mediantdevice 
+
---
+++ scripts/example-4.ps1
@@ -0,0 +1,10 @@
+﻿#requires -version 5.0
+#requires -module Mediant
+
+import-module mediant 
+
+$mediantcredential = Get-Credential 
+
+$mediantdevice = Get-MediantDevice -Mediant 172.30.69.146 -Credential $mediantcredential -Http http
+
+Get-MediantDeviceStatus -MediantDevice $mediantdevice 
