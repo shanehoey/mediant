4fb48d2554e818a722984fce23eac691f9bfbac1
shanehoey <shanehoey@users.noreply.github.com>
2018-01-08 23:20:43 +1000

    added build script

---
+++ scripts/build.ps1
@@ -0,0 +1,35 @@
+exit
+
+set-location $env:USERPROFILE\github\mediant
+
+$NuGetApiKey = $NuGetApiKey
+$NuGetApiKey
+
+$cert = Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert
+$cert | format-table subject,issuer
+
+$version = "1.0.0"
+
+Update-ModuleManifest -Path ".\mediant\mediant.psd1" -ModuleVersion $version
+
+Set-AuthenticodeSignature -filepath ".\mediant\mediant.psd1" -Certificate $cert
+(Get-AuthenticodeSignature -FilePath ".\mediant\mediant.psd1").Status
+
+Set-AuthenticodeSignature -filepath ".\mediant\mediant.psm1" -Certificate $cert
+(Get-AuthenticodeSignature -FilePath ".\mediant\mediant.psm1").Status
+
+Test-ModuleManifest -path ".\mediant\mediant.psd1"
+
+Remove-Module mediant -ErrorAction SilentlyContinue
+Import-Module .\mediant\mediant.psd1 
+
+get-command -Module mediant | select name,version
+
+#Manually run these 
+code .\Scripts\example-1.ps1
+
+### MANUAL GitHUB Commit to master
+
+### IMPORTANT ONLY RUN AFTER ALL ABOVE IS COMPLETED
+pause
+Publish-Module -path .\mediant -NuGetApiKey $NuGetApiKey
