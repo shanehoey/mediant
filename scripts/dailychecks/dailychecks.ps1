9e870b1ab5e37914f138ac779f1b17a8a031b05f
Shane Hoey <git@shanehoey.com>
2019-02-10 09:39:58 +1000

    updated script

--- scripts/dailychecks/dailychecks.ps1
+++ scripts/dailychecks/dailychecks.ps1
@@ -1,28 +1,27 @@
 import-module .\mediant\mediant.psm1 -force
 
-$mediantdevices = foreach ($mediant in (get-content -Path .\scripts\mediant.json | convertfrom-json) ) { 
-    Get-MediantDevice -Mediant $mediant.fqdn -http https -username $mediant.username -password (ConvertTo-SecureString -String $mediant.password)
+$mediantdevices = foreach ($mediant in (get-content -Path .\dailychecks.json | convertfrom-json) ) { 
+    Get-MediantDevice -Mediant $mediant.mediant -http https -username $mediant.username -password (ConvertTo-SecureString -String $mediant.password)
 }
 
 foreach ($mediant in $mediantdevices) {
+    $filename = "$((get-date).tostring("dd_MM_yyyy"))_$($mediant.mediant).txt"
     $script = "show system ntp-status"
-    $output = (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show high-availability status"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show network physical-port"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show network route"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show running-config troubleshoot"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show system uptime"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show system version"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show voip proxy set status"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
     $script = "show voip register-account"
-    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
-    $filename = "daily_$($mediant.fqdn).txt"
-    out-file -filepath $filename
+    (Invoke-MediantCurlRequest -Mediant $mediant.mediant -Credential $mediant.credential -script $script)  | ConvertFrom-Json  | Select-Object -expand output | out-file -filepath $filename -Append
 }
