8fe2f5d9d6b0d0c06256948959156de73208b075
Shane Hoey <git@shanehoey.com>
2019-02-10 09:14:05 +1000

    .

--- scripts/dailychecks/dailychecks.ps1
+++ scripts/dailychecks/dailychecks.ps1
@@ -1,23 +1,28 @@
-import module .\mediant\mediant.psm1  force
+import-module .\mediant\mediant.psm1 -force
 
-$mediant   =  "sbc1.aclearning.com.au"
-$credential = get credential
+$mediantdevices = foreach ($mediant in (get-content -Path .\scripts\mediant.json | convertfrom-json) ) { 
+    Get-MediantDevice -Mediant $mediant.fqdn -http https -username $mediant.username -password (ConvertTo-SecureString -String $mediant.password)
+}
 
-$script = "show system ntp-status"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show high-availability status"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show network physical port"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show network route"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show running config troubleshoot"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show system uptime"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show system version"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show voip proxy set status"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
-$script = "show voip register account"
-Invoke-MediantScript -Mediant $mediant -Credential $credential -script $script
+foreach ($mediant in $mediantdevices) {
+    $script = "show system ntp-status"
+    $output = (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show high-availability status"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show network physical-port"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show network route"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show running-config troubleshoot"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show system uptime"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show system version"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show voip proxy set status"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $script = "show voip register-account"
+    $output += (Invoke-MediantCurlRequest -Mediant $mediant.fqdn -Credential $mediant.credential -script $script) 
+    $filename = "daily_$($mediant.fqdn).txt"
+    out-file -filepath (daily.txt -Encoding ascii -
+}
